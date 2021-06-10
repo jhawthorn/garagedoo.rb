c31f5023ea532d46f2c18e49ffba8f53ba5af1b3
John Hawthorn <john@hawthorn.email>
2021-06-09 20:53:05 -0700

    Working initial version

--- garagedoo.rb
+++ garagedoo.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-#require "mini_gpio"
+require "mini_gpio"
+require "erb"
+require "uri"
 
 class GarageDoor
   def initialize
+    @gpio = MiniGPIO.new
+    @toggle_pin = 2
   end
 
   def call(env)
@@ -12,17 +16,33 @@ class GarageDoor
 
     case [method, path]
     when ["GET", "/"]
-      index
+      index(env)
+    when ["POST", "/toggle"]
+      toggle(env)
     else
-      not_found
+      not_found(env)
     end
   end
 
-  def index
-    [200, {}, ["HELLO WORLD"]]
+  Dir["views/*.html.erb"].each do |filename|
+    basename = File.basename(filename, ".html.erb")
+    erb = ERB.new(File.read("views/index.html.erb"))
+    erb.def_method(self, "render_#{basename}(env)", filename)
   end
 
-  def not_found
+  def index(env)
+    [200, {}, [render_index(env)]]
+  end
+
+  def toggle(env)
+    @gpio.set_mode @toggle_pin, MiniGPIO::Modes::OUTPUT
+    @gpio.write @toggle_pin, 0
+    sleep 1
+    @gpio.set_mode @toggle_pin, MiniGPIO::Modes::INPUT
+    [302, { "Location" => "/" }, []]
+  end
+
+  def not_found(env)
     [404, {}, ["404: not found"]]
   end
 end
---
+++ start.sh
@@ -0,0 +1,1 @@
+/home/pi/.gem/ruby/2.5.0/bin/rackup -p 3000 -o 0.0.0.0
